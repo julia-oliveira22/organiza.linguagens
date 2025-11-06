fdaf310bcda7468131bfce3b03a40b97c56a2d81
julia <julia.o.silva23@aluno.senai.br>
2025-11-06 15:55:00 -0300

    teste final parte1

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function Julinha(){
-let nome= prompt("Digite seu Nome:");
-let numero= prompt("Digite um numero:");
-let comida= prompt("Digite sua comida favorita:");
-let CPF= prompt("Digite seu CPF:");
-let musica= prompt("Digite seu estilo de musica favorita");
-let artista=prompt("Digite seu artista favorito:");
-alert("Esse é seu Nome: " + nome)
-alert("Esse é seu Número: " + numero)
-alert("Essa é sua Comida Favorita: " + comida)
-alert("Esse é seu CPF: " + CPF)
-alert("Essa é sua Música Favorita: " + musica)
-alert("Esse é sue Artista Favorito: " + artista)
+    let nome= prompt("Digite seu Nome:");
+    let numero= prompt("Digite um numero:");
+    let comida= prompt("Digite sua comida favorita:");
+    let CPF= prompt("Digite seu CPF:");
+    let musica= prompt("Digite seu estilo de musica favorita");
+    let artista=prompt("Digite seu artista favorito:");
+    
+    alert("Esse é seu Nome: " + nome)
+    alert("Esse é seu Número: " + numero)
+    alert("Essa é sua Comida Favorita: " + comida)
+    alert("Esse é seu CPF: " + CPF)
+    alert("Essa é sua Música Favorita: " + musica)
+    alert("Esse é sue Artista Favorito: " + artista)
 }
